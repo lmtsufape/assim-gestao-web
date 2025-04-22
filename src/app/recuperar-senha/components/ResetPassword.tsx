0cca4acf84d78070979120c300ebd106f72f34e6
Julio Neto <nettojulio@hotmail.com>
2025-04-21 21:38:40 -0300

    (fix):update ResetPassword component to use Snackbar for messages and improve user feedback

--- src/app/recuperar-senha/components/ResetPassword.tsx
+++ src/app/recuperar-senha/components/ResetPassword.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 
 import S from './styles.module.scss';
@@ -8,40 +9,67 @@ import Button from '@/components/Button';
 import Input from '@/components/Input';
 
 import { sendResetPasswordEmail } from '@/services';
+import { Alert, AlertTitle, Snackbar } from '@mui/material';
 
 export const ResetPasswordRequest = () => {
   const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const router = useRouter();
 
   const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     try {
       await sendResetPasswordEmail(email);
-      setMessage('Email de redefinição de senha enviado com sucesso.');
+      setSuccessMessage('Email de redefinição de senha enviado com sucesso.');
+      setTimeout(() => {
+        router.push('/');
+      }, 7000);
     } catch (error) {
       console.debug(error);
-      setMessage('Falha ao enviar o email de redefinição de senha.');
+      setErrorMessage('Falha ao enviar o email de redefinição de senha.');
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className={S.form}>
-      <div className={S['form-group']}>
-        <label htmlFor="email">E-mail</label>
-        <Input
-          placeholder="contato@email.com"
-          name="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div className={S['button-container']}>
-        <Button type="submit" dataType="filled">
-          Recuperar Senha
-        </Button>
-      </div>
-      {message && <p>{message}</p>}
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className={S.form}>
+        <div className={S['form-group']}>
+          <label htmlFor="email">E-mail</label>
+          <Input
+            placeholder="contato@email.com"
+            name="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className={S['button-container']}>
+          <Button type="submit" dataType="filled">
+            Recuperar Senha
+          </Button>
+        </div>
+      </form>
+      <Snackbar
+        open={successMessage.length > 0}
+        autoHideDuration={6000}
+        onClose={() => setSuccessMessage('')}
+      >
+        <Alert variant="filled" severity="success">
+          <AlertTitle>Sucesso!</AlertTitle>
+          {successMessage}
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={errorMessage.length > 0}
+        autoHideDuration={6000}
+        onClose={() => setErrorMessage('')}
+      >
+        <Alert variant="filled" severity="error">
+          <AlertTitle>Erro!</AlertTitle>
+          {errorMessage}
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
